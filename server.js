d176dccea1f1418815823ddcf0dc9ad1fd76df8b
NachtBlade31 <kbgoku@gmail.com>
2019-12-25 18:08:44 +0530

    adding databse connection and api routes for user,post,profile and auth

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
+//Connect database
+connectDB();
 app.get("/", (req, res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started at port ${port}`));
