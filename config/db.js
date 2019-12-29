228e83cf8347ab777dd4652104156be18f69e6e4
NachtBlade31 <kbgoku@gmail.com>
2019-12-30 01:33:13 +0530

    getting all user, getting profile byid  and fixed the naame and  avatar uissue

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
         });
         console.log("MongoDB Connection Established .......")
     }
--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const newProfile = {
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'User'
     },
     company: {
         type: String
