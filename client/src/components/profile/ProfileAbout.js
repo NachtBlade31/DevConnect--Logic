c058df7e2c32214fe3ba9e1b79c834d567f19978
NachtBlade31 <kbgoku@gmail.com>
2020-02-09 03:01:22 +0530

    profile education and getProfilebyid

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -9,17 +9,17 @@ const ProfileAbout = ({ profile: {
     return (
         <div class="profile-about bg-light p-2">{
             bio && (<div>
-                <h2 class="text-primary">{name.trim().split(' ')[0]}'s Bio</h2>
+                <h2 className="text-primary">{name.trim().split(' ')[0]}'s Bio</h2>
                 <p>{bio}</p>
-                <div class="line"></div>
+                <div className="line"></div>
             </div>)
         }
 
-            <h2 class="text-primary">Skill Set</h2>
-            <div class="skills">
+            <h2 className="text-primary">Skill Set</h2>
+            <div className="skills">
                 {skills.map((skill, index) => (
-                    <div key={index} class="p-1">
-                        <i class="fas fa-check"></i>
+                    <div key={index} className="p-1">
+                        <i className="fas fa-check"></i>
                         {skill}
                     </div>
                 ))}
