aaa9a8c06ed5dc09ca985211b2f5cdce13be727a
NachtBlade31 <kbgoku@gmail.com>
2020-02-10 22:44:18 +0530

    new changes for production

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 //Connect database
 connectDB();
-app.get("/", (req, res) => res.send('API running'));
+
 
 
 //Init Middleware
@@ -15,5 +16,15 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+//Serve Static asset in production
+if (process.env.NODE_ENV === 'production') {
+    //SET STATIC FOLDER
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started at port ${port}`));
